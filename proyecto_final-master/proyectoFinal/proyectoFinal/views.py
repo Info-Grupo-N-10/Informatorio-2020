19c426f81f9e4a689a26f20f554e161b65073b44
Chicho <chickyaguirre1@gmail.com>
2020-09-30 15:57:45 -0300

    lo ultimo xd

--- proyecto_final-master/proyectoFinal/proyectoFinal/views.py
+++ proyecto_final-master/proyectoFinal/proyectoFinal/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.urls import reverse_lazy
 
-from apps.favoritos.models import Favoritos
+from apps.usuarios.models import Usuario
 from apps.publicacion.models import Publicaciones
 from django.views.generic.list import ListView
+
+import random
 
 # class Home(ListView):
 #     model = Publicaciones
@@ -16,12 +18,15 @@ from django.views.generic.list import ListView
 #         return context
 
 def Home(request):
+    u = list(Usuario.objects.all())
     p = Publicaciones.objects.all()
-    f = Favoritos.objects.all()
+    r = random.sample(u, 3)
 
     context = {
     'publis': p,
-    'favs': f, }
+    'usuarios': r, }
+
+    return render(request, 'home.html', context)
 
     return render(request, 'home.html', context)
 
